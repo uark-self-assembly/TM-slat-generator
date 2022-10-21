cc4aadcf055a245f000476d55a72358f7f79b686
Jackson Bullard <jsbullar@uark.edu>
2022-10-21 15:38:32 -0500

    The way vertical tiles are serialized by the slat generator is now much more intuitive

--- assemblies/xml_to_scadnano.py
+++ assemblies/xml_to_scadnano.py
@@ -1,13 +1,16 @@
 from ast import literal_eval as make_tuple
 import warnings
 from lxml import etree as ET
+import scadnano as sc
 import sys
 from typing import Union
 from utils.slats import CrissCrossSlat, CrissCrossStaple
 
-# TODO
-# Notes: z=0 (vertical) slats start one coordinate northward to their translation
-#		 z=-1 (horizontal) slats actually make sense
+# System constants
+# TODO: Take user input
+EIGHT_DOMAIN_OFFSET = 40
+TWELVE_DOMAIN_OFFSET = int(EIGHT_DOMAIN_OFFSET * 1.5)
+FOUR_DOMAIN_OFFSET = EIGHT_DOMAIN_OFFSET // 2
 
 """
 sys_elem (PolyominoSystem) has 4 children:
@@ -18,17 +21,19 @@ sys_elem (PolyominoSystem) has 4 children:
 
 TODO: Document!
 """
-def GetPolyominoTypes(filename: str) -> list[Union[CrissCrossSlat, CrissCrossStaple]]:
+def GetPolyominoTypes(filename: str) -> dict[str, Union[CrissCrossSlat, CrissCrossStaple]]:
 
-	slats: list[Union[CrissCrossSlat, CrissCrossStaple]] = []
+	name_to_slat: dict[str, Union[CrissCrossSlat, CrissCrossStaple]] = dict()
 	xml_tree = ET.parse(filename, None)
 	polyomino_types = xml_tree.getroot()[1]
 
 	for poly_type in polyomino_types:
 		
-		slats.append(BuildSlat(poly_type))
+		# Note: If a name is duplicated, this will overwrite a polyomino with another
+		slat = BuildSlat(poly_type)
+		name_to_slat[slat.name] = slat
 
-	return slats
+	return name_to_slat
 
 """
 Can't set type of param: polyomino_type because ElementTree is unclear.
@@ -68,7 +73,8 @@ def BuildSlat(polyomino_type) -> Union[CrissCrossSlat, CrissCrossStaple]:
 		coord_to_domain[coords[:2]] = domain
 
 	# Check if this is a staple/"frontier tooth"
-	if (0, 8) in coord_to_domain and (1, 8) in coord_to_domain:
+	if (0, 1) in coord_to_domain and (1, 1) in coord_to_domain:	# TODO: Changed here and below elif
+		
 		domains = list()
 
 		# Two x-values (a left and a right side)
@@ -81,18 +87,63 @@ def BuildSlat(polyomino_type) -> Union[CrissCrossSlat, CrissCrossStaple]:
 		return CrissCrossStaple(name, domains)
 
 	# Check if this is a vertical slat
-	elif (0, 8) in coord_to_domain:
-		# Vertical slat coordinates start at 1 and go to 8
-		domains = [coord_to_domain[(0, y)] for y in range(1, len(coord_to_domain) + 1)]
+	elif (0, 1) in coord_to_domain:
+		# Iterate through y component
+		domains = [coord_to_domain[(0, y)] for y in range(len(coord_to_domain))]
 
 		return CrissCrossSlat(name, 'N', domains)
 
 	# This is a horizontal slat
 	else:
-		# Horizontal slat coordinates behave nicely (start at zero, end at len - 1)
+		# Iterate through x component
 		domains = [coord_to_domain[(x, 0)] for x in range(len(coord_to_domain))]
 
 		return CrissCrossSlat(name, 'E', domains)
+
+"""
+TODO: Document!
+"""
+def BuildScadnanoSystem(
+	slats: dict[str, Union[CrissCrossSlat, CrissCrossStaple]],
+	input_file: str
+) -> sc.Design:
+
+	# Access to <Polyominoes> (the child of <assembly>, which is a child of <PolyominoSystem>)
+	xml_polyominoes = ET.parse(input_file, None).getroot()[3][0]
+	# num_helices, max_offset = GetSystemBounds(slats, xml_polyominoes)
+	
+	# TODO: Create "helices"
+		# How many? Need to read assembly to find out...
+
+	# TODO: Place slats one-by-one
+
+	# TODO: Insertions
+
+"""
+Returns the number of helices and max offset needed for the scadnano system.
+"""
+def GetSystemBounds(
+	slats: dict[str, Union[CrissCrossSlat, CrissCrossStaple]],
+	xml_polyominoes
+) -> tuple[int, int]:
+
+	num_helices: int = 0	# Number of "rows" in scadnano system
+	max_offset: int = 0		# Maximum distance on the x-axis in scadnano system
+	
+	for polyomino in xml_polyominoes:
+		
+		# The what (polyomino type) and the where (translation)
+		name: str = polyomino[0].text
+		translation: tuple[int, int, int] = make_tuple(polyomino[1].text)
+
+		slat = slats[name]
+		# Vertical slat or frontier tooth/staple
+		if (slat.orientation == 'N'):
+			# if (num_helices)	TODO: Left off here
+			pass
+		# Horizontal slat
+		else:
+			pass
 
 if __name__ == '__main__':
 
@@ -101,4 +152,11 @@ if __name__ == '__main__':
 		print('Please provide exactly one argument: filename.xml!')
 		exit()
 
-	slats = GetPolyominoTypes(sys.argv[1])
+	# Name of .xml assembly file
+	input_file = sys.argv[1]
+
+	# Get slat types from XML file
+	slats = GetPolyominoTypes(input_file)
+
+	# Create scadnano file from assembly
+	BuildScadnanoSystem(slats, input_file)
